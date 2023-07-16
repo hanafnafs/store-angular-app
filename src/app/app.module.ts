faeb64cf29684e05dea04c7da617a0fe665c6784
hanafnafs <hanafnafs@gmail.com>
2023-07-16 22:33:10 +0300

    finalizing login design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // NgbModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
